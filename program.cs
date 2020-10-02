818266f5fb1cb937d47dc7ebfa81382c3f8c778a
josiemcateer <71390425+josiemcateer@users.noreply.github.com>
2020-10-02 10:36:45 +0100

    added answers

--- program.cs
+++ program.cs
@@ -5,11 +5,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             task t = new task();
-            t.inttochar(789);
-            t.inttochar(1005);
-            t.inttochar(9784);
-            t.inttochar(100005);
-            t.inttochar(-100);
+            t.inttochar(789);    // seven hundred and eighty nine
+            t.inttochar(1005);  // one thousand and five
+            t.inttochar(9784);     // nine thousand seven hundred and eighty four
+            t.inttochar(100005);    // invalid number
+            t.inttochar(-100);     // invalid number
             Console.Read();
         }
     }

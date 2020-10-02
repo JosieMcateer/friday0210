dfe44a8508dc29e0872483d80834afb49cc5faea
josiemcateer <71390425+josiemcateer@users.noreply.github.com>
2020-10-02 09:52:53 +0100

    testing numbers

---
+++ program.cs
@@ -0,0 +1,15 @@
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task t = new task();
+            t.inttochar(789);
+            t.inttochar(1005);
+            t.inttochar(9784);
+            t.inttochar(100005);
+            Console.Read();
+        }
+    }
+}

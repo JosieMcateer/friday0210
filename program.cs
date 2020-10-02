73b9cf090a4c81e7848d19c8e564e9ceb173d566
josiemcateer <71390425+josiemcateer@users.noreply.github.com>
2020-10-02 10:38:30 +0100

    -

--- program.cs
+++ program.cs
@@ -5,9 +5,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             task t = new task();
-            t.inttochar(789);    // seven hundred and eighty nine
-            t.inttochar(1005);  // one thousand and five
-            t.inttochar(9784);     // nine thousand seven hundred and eighty four
+            t.inttochar(789);    // seven hundred  eighty nine
+            t.inttochar(1005);  // one thousand five
+            t.inttochar(9784);     // nine thousand seven hundred eighty four
             t.inttochar(100005);    // invalid number
             t.inttochar(-100);     // invalid number
             Console.Read();
